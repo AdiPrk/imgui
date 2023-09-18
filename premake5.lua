0b5e65a08fbf36cb2b35ec6bae1909bcc278825f
AdiPrk <adityaprk@outlook.com>
2023-09-18 13:41:01 -0700

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,20 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C"
+    
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+	    "**.h",
+		"**.c"
+	}
+    
+	filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+		cppdialect "C++20"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
